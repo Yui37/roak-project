9bfc37bf0fd612cb55b0dc8ec7e81d4cefcf372e
Yui <ytkakentan@yahoo.co.jp>
2021-07-04 12:45:14 +0900

    add responsive to header news

---
+++ single.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>地球雑貨　ろーく</title>
+        <meta name="description" content="import shop in Higashihiroshima">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous"> -->
+        <script src="https://kit.fontawesome.com/779dc48fa9.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="single-page-top-wrapper full-width-container">
+            <div class="single-page-top-texts column-5">
+                <div class="single-page-top-text single-page-sub-title">
+                    <p>アジアの色・香・味を楽しめる癒しの空間</p>
+                </div>
+                <div class="single-page-top-text single-page-main-title">
+                    <h2>地球雑貨</h2>
+                    <h1>ろーく</h1>
+                </div>
+            </div>
+        </div>
+        <header>
+            <nuv class="menu-bar">
+                <div class="nomal-menu">
+                    <div class="menu-container">
+                        <ul class="menus">
+                            <li class="menu home">
+                                <a href="" class="text-underline-hover">
+                                    <h1>Home</h1>
+                                    <p>ホーム</p>
+                                </a>
+                            </li>
+                            <li class="menu news-event">
+                                <a href="" class="text-underline-hover">
+                                    <h1>News & Event</h1>
+                                    <p>お知らせ</p>
+                                </a>
+                            </li>
+                            <li class="menu concept">
+                                <a href="" class="text-underline-hover">
+                                    <h1>Concept</h1>
+                                    <p>コンセプト</p>
+                                </a>
+                            </li>
+                            <li class="menu service">
+                                <a href="" class="text-underline-hover">
+                                    <h1>Service</h1>
+                                    <p>お取り扱い</p>
+                                </a>
+                            </li>
+                            <li class="menu location">
+                                <a href="" class="text-underline-hover">
+                                    <h1>Location</h1>
+                                    <p>店舗案内</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="hamburger-menu">
+                    <!-- <div class="collapse" id="navbarToggleExternalContent">
+                        <div class="bg-dark p-4">
+                          <h5 class="text-white h4">Collapsed content</h5>
+                          <span class="text-muted">Toggleable via the navbar brand.</span>
+                        </div>
+                    </div>
+                    <nav class="navbar navbar-dark bg-dark">
+                        <div class="container-fluid">
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                        </div>
+                    </nav> -->
+                </div>
+            </nuv>
+        </header>
+        <main>
+            <div class="single-news-wrapper full-width-container">
+                <div class="single-news-top full-width-container">
+                    <h1 class="single-contents-main-title">News & Event</h1>
+                    <h2 class="single-contents-sub-title">お知らせ</h2>
+                    <a　ref="#" class="return-to-toppage">←　Back</a>
+                </div>
+                <div class="single-news-main full-width-container">
+                    <div class="single-post column-9">
+                        <h1 class="single-post-title">3月31日まで香り＋1点プレゼント(*´ω｀*)</h1>
+                        <img src="images/o1080071814936976761.jpg">
+                        <h2 class="single-post-category">イベント</h2>
+                        <div class="post-tags">
+                            <p class="post-tag">アジアン雑貨</p>
+                            <p class="post-tag">セラピー</p>
+                            <p class="post-tag">ハロウィーン</p>
+                            <p class="post-tag">天然石</p>
+                        </div>
+                        <h2 class="single-post-date">2020.1.30</h2>
+                        <P class="single-post-content">現在、この世界に「香り」は数十万種類あると言われています。なんと人類は数千年前から「香り」の持つ効能を扱ってきました。<br>例えば、約2000年前。古代エジプトの女王で有名なクレオパトラは貴重な素材を集めさせて香水を作らせ愛用していたとされます。研究者によりそのレシピが解読されて再現されたそうですよ…(￣□￣;)<br>ミルラベースだったそうなので、少し濃厚かもしれませんね。<br>古代において、香水は贅沢品というよりも儀式、医療、治療など生活だけでなく死後の世界に向けても深い意味を持ち使用されていたとされています。<br>現代、人類における嗅覚のメカニズムの研究により科学的に「効能」「効果」が解明が進んでいるそうです.<br>香りは脳にダイレクトに働きかけます。ですので、好きな香り、嫌いな香りは皆さん感じやすく一緒に暮らしている家族でも大きく異なることもよくありますね。<br>好みの香りを日常へ取り入れることでリラックスだけでなく気分のコントロール、ストレスの軽減にもなります(*´ω｀*)</P>
+                        <a class="like-btn">
+                            <div class="like-icon">
+                                <i class="fas fa-heart"></i>
+                            </div>
+                            <div class="like-text">
+                                <p>like</p>
+                            </div>
+                            <div class="like-count">
+                                <p>6</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="side-bar column-3">
+                        <div class="side-bar-title">
+                            <h2>More</h2>
+                        </div>
+                        <div class="side-bar-contents">
+                            <div class="side-bar-content">
+                                <a href="#" class="text-underline-hover">
+                                    <img src="images/o1080072014911165029.jpeg" class="side-content-img">
+                                    <div class="side-content-text">
+                                        <h2 class="post-category">イベント</h2>
+                                        <h2 class="post-date">2020.1.30</h2>
+                                        <h1 class="side-post-title">3月31日まで香り＋1点プレゼント(*´ω｀*)</h1>
+                                        <P>現在、この世界に「香り」は数十万種類あると言われています。なんと人類は数千年前から「香り」の持つ効能を扱ってきました。例えば、約2000年前。古代エジプトの…</P>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="side-bar-content">
+                                <a href="#" class="text-underline-hover">
+                                    <img src="images/o1080072014911165029.jpeg" class="side-content-img">
+                                    <div class="side-content-text">
+                                        <h2 class="post-category">イベント</h2>
+                                        <h2 class="post-date">2020.1.30</h2>
+                                        <h1 class="side-post-title">3月31日まで香り＋1点プレゼント(*´ω｀*)</h1>
+                                        <P>現在、この世界に「香り」は数十万種類あると言われています。なんと人類は数千年前から「香り」の持つ効能を扱ってきました。例えば、約2000年前。古代エジプトの…</P>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="side-bar-content">
+                                <a href="#" class="text-underline-hover">
+                                    <img src="images/o1080072014911165029.jpeg" class="side-content-img">
+                                    <div class="side-content-text">
+                                        <h2 class="post-category">イベント</h2>
+                                        <h2 class="post-date">2020.1.30</h2>
+                                        <h1 class="side-post-title">3月31日まで香り＋1点プレゼント(*´ω｀*)</h1>
+                                        <P>現在、この世界に「香り」は数十万種類あると言われています。なんと人類は数千年前から「香り」の持つ効能を扱ってきました。例えば、約2000年前。古代エジプトの…</P>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="side-bar-content">
+                                <a href="#" class="text-underline-hover">
+                                    <img src="images/o1080072014911165029.jpeg" class="side-content-img">
+                                    <div class="side-content-text">
+                                        <h2 class="post-category">イベント</h2>
+                                        <h2 class="post-date">2020.1.30</h2>
+                                        <h1 class="side-post-title">3月31日まで香り＋1点プレゼント(*´ω｀*)</h1>
+                                        <P>現在、この世界に「香り」は数十万種類あると言われています。なんと人類は数千年前から「香り」の持つ効能を扱ってきました。例えば、約2000年前。古代エジプトの…</P>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="side-bar-content">
+                                <a href="#" class="text-underline-hover">
+                                    <img src="images/o1080072014911165029.jpeg" class="side-content-img">
+                                    <div class="side-content-text">
+                                        <h2 class="post-category">イベント</h2>
+                                        <h2 class="post-date">2020.1.30</h2>
+                                        <h1 class="side-post-title">3月31日まで香り＋1点プレゼント(*´ω｀*)</h1>
+                                        <P>現在、この世界に「香り」は数十万種類あると言われています。なんと人類は数千年前から「香り」の持つ効能を扱ってきました。例えば、約2000年前。古代エジプトの…</P>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="side-bar-content">
+                                <a href="#" class="text-underline-hover">
+                                    <img src="images/o1080072014911165029.jpeg" class="side-content-img">
+                                    <div class="side-content-text">
+                                        <h2 class="post-category">イベント</h2>
+                                        <h2 class="post-date">2020.1.30</h2>
+                                        <h1 class="side-post-title">3月31日まで香り＋1点プレゼント(*´ω｀*)</h1>
+                                        <P>現在、この世界に「香り」は数十万種類あると言われています。なんと人類は数千年前から「香り」の持つ効能を扱ってきました。例えば、約2000年前。古代エジプトの…</P>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="side-bar-blog-link">
+                            <div class="container">
+                                <div class="side-bar-title">
+                                    <h2>Blog</h2>
+                                </div>
+                            </div>
+                            <div class="blog-links">
+                                <div class="side-blog-link owner-blog">
+                                    <a href="#" class="text-underline-hover">
+                                        <img src="images/img004.jpg">
+                                        <p>Owner Blog</p>
+                                    </a>
+                                </div>
+                                <div class="side-blog-link kaori-blog">
+                                    <a href="#" class="text-underline-hover">
+                                        <img src="images/9F65.tmp.jpg">
+                                        <p>Kaori Blog</p>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        </sidebar>
+        <footer>
+            <div class="footer-wrapper">
+                <div class="footer-container">
+                    <div class="shop-title">
+                        <div class="shop-sub-title">
+                            <p>アジアの色・香・味を楽しめる癒しの空間</p>
+                        </div>
+                        <div class="shop-main-title">
+                            <h2>地球雑貨</h2>
+                            <h1>ろーく</h1>
+                        </div>
+                    </div>
+                    <div class="menu-and-logo">
+                        <div class="menu-list">
+                            <p>ホーム</p>
+                            <p>お知らせ</p>
+                            <p>コンセプト</p>
+                            <p>お取り扱い</p>
+                            <p>店舗案内</p>
+                        </div>
+                        <img src="images/20210501_173253_983.jpg " class="logo">
+                    </div>
+                    <div class="sns-icons">
+                        <a href="#" class="text-underline-hover"><i class="fab fa-instagram sns-icon instagram"></i></a>
+                        <div class="facebook owner-facebook">
+                            <a href="#" class="text-underline-hover"><i class="fab fa-facebook-f sns-icon"></i></a>
+                            <p>Owner</p>
+                        </div>
+                        <div class="facebook kaori-facebook">
+                            <a href="#" class="text-underline-hover"><i class="fab fa-facebook-f sns-icon"></i></a>
+                            <p>Kaori</p>
+                        </div>
+                    </div>
+                    <div class="copyright">
+                        <p>Copyright © chikyuzakka Loac</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script> -->
+    </body>
+</html>
